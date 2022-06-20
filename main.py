73947e27a7552ea137d372ce26743818c305a404
BlueDespero <cezarytroska@gmail.com>
2022-06-20 20:31:34 +0200

    top_p_filtering added to the main part

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import torch
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def top_p_filtering(logits, top_p=0.0, filter_value=-float('Inf')):
+    assert logits.dim() == 1  # batch size 1 for now - could be updated for more but the code would be less clear
+
+    if top_p > 0.0:
+        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+        cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+        # Remove tokens with cumulative probability above the threshold
+        sorted_indices_to_remove = cumulative_probs > top_p
+        # Shift the indices to the right to keep also the first token above the threshold
+        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+        sorted_indices_to_remove[..., 0] = 0
+
+        indices_to_remove = sorted_indices[sorted_indices_to_remove]
+        logits[indices_to_remove] = filter_value
+    return logits
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def word_generating_procedure():
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Here is how to use this function for top-p sampling
+temperature = 1.0
+top_p = 0.9
+
+# Get logits with a forward pass in our model (input is pre-defined)
+logits = model(input)
+
+# Keep only the last token predictions of the first batch item (batch size 1), apply a temperature coefficient and filter
+logits = logits[0, -1, :] / temperature
+filtered_logits = top_k_top_p_filtering(logits, top_p=top_p)
+
+# Sample from the filtered distribution
+probabilities = F.softmax(filtered_logits, dim=-1)
+next_token = torch.multinomial(probabilities, 1)
